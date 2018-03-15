c802fecbaf5fefc8e92ccb0902569d61823489b5
Temo Bochorishvili <bochorishvili.teimuraz@gtu.ge>
2018-03-15 15:32:16 +0400

    Maps Test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef, OnChanges } from '@angular/core';
+import { tileLayer, latLng, circle, polygon, marker, Map, LeafletMouseEvent, icon } from 'leaflet';
+import { delay } from 'q';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  map: Map;
+  checkPin: boolean = false;
+  removeLayers: boolean = false;
+
+  checkPinText: string = 'Enable putting pins';
+  removeLayersText: string = 'Allow to remove ';
+
+  options = {
+    layers: [
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 10,
+    center: latLng(41.716667, 44.783333),
+  };
+
+  layersControl = {
+    // baseLayers: {
+    //   'Open Street Map': tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
+    //   'Open Cycle Map': tileLayer('http://{s}.tile.opencyclemap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    // },
+    overlays: {
+      'Big Circle': circle([41.8, 44.8], { radius: 5000 }),
+      'Big Square': polygon([[41.765, 44.745], [41.7, 44.789], [41.656, 44.545], [41.5, 44.5]])
+    }
+  }
+
+  layers = [
+    circle([41.716667, 44.783333], { radius: 5000 }),
+    polygon([[41.765, 44.745], [41.7, 44.789], [41.656, 44.545]]),
+    marker([46.879966, -121.726909])
+  ];
+
+  constructor(private changeDetector: ChangeDetectorRef) { }
+
+  ngOnInit() {
+    
+  }
+
+  onCheckPin(){
+    this.checkPin = !this.checkPin;
+    this.checkPinText = this.checkPin ? 'Disable putting pins' : 'Enable putting pins';
+  }
+
+  onRemoveLayerClick(){
+
+  }
+
+  onMapReady(map: Map) {
+    this.map = map;
+  }
+
+  onMapClick() {
+    if (this.checkPin) {
+      this.map.once('click', (event: LeafletMouseEvent) => {
+        console.log(event.latlng);
+        this.layers.push(marker([event.latlng.lat, event.latlng.lng], {
+          draggable: true,
+          icon: icon({
+            iconSize: [25, 41],
+            iconAnchor: [13, 41],
+            iconUrl: 'assets/marker-icon.png',
+            shadowUrl: 'assets/marker-shadow.png',
+          })
+
+        }).bindPopup("as " + event.latlng.lng)
+      );
+      });
+
+      setTimeout(() => {
+
+      },
+        100);
+    }
+    if(this.removeLayers){
+      this.layers.forEach(ass => {
+        ass.once('click', (event: LeafletMouseEvent) => {
+          this.map.removeLayer(ass);
+        })
+      })
+    }
+  }
+  // 
+
 }
